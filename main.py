0b61c5392dd08b6aaad4faa14cf5093ed61803dd
Vince Capodanno <vcap51@yahoo.com>
2021-04-11 17:07:56 -0400

    initial refactor of node processing methods

--- main.py
+++ main.py
@@ -2,27 +2,8 @@ import plotly.graph_objects as go
 import csv
 from operator import itemgetter
 
-
-class PlayerCareer:
-
-    def __init__(self, player_id, player_name, career_phase):
-        self.__player_id = player_id
-        self.__player_name = player_name
-        self.__player_team_names = [career_phase["nickname"]]
-        self.__career_phases = [career_phase]
-
-    def append_career_phase(self, career_phase):
-        self.__career_phases.append(career_phase)
-        self.__player_team_names.append(career_phase["nickname"])
-
-    def get_player_name(self):
-        return self.__player_name
-
-    def was_player_ever_on_team(self, team_searched):
-        return team_searched in self.__player_team_names
-
-    @staticmethod
-    def node_x_and_labels_method(_is_first, __current_career_phase):
+# TODO: refactor out the code that cleans the data so this function only generates the objects and labels
+def create_x_axis_nodes_and_labels(_is_first, __current_career_phase):
         _label = ''
         _team = __current_career_phase["nickname"]
 
@@ -63,63 +44,83 @@ class PlayerCareer:
             _label = ""
         return _x_position_dictionary, _label
 
-    def export_nodes(self, _index_of_last_node_added, _current_season_and_day):
-        _node_labels = []
-        _node_x_position_dictionaries = []
-        _node_y_position_dictionaries = []
-        _node_colors = []
-        _link_sources = []
-        _link_targets = []
-        _link_values = []
-        _link_colors = []
-        _phase_count = 0
-        _new_index = 0
-        for __current_career_phase in self.__career_phases:
+# TODO: comment explaining what each of these returned objects do
+def export_nodes(__career_phases, _index_of_last_node_added, _current_season_and_day):
+    _node_labels = []
+    _node_x_position_dictionaries = []
+    _node_y_position_dictionaries = []
+    _node_colors = []
+    _link_sources = []
+    _link_targets = []
+    _link_values = []
+    _link_colors = []
+    _phase_count = 0
+    _new_index = 0
+    for __current_career_phase in __career_phases:
 
-            # each career phase is a pair of nodes (start and end) with a link between,
-            # a span where they were on one team
-            _color = __current_career_phase["team_main_color"]
-            _color2 = __current_career_phase["team_secondary_color"]
-            _y_position_dictionary = {"team_name": __current_career_phase["nickname"],
-                                      "position_type_id": __current_career_phase["position_type_id"],
-                                      "position_id": __current_career_phase["position_id"]}
+        # each career phase is a pair of nodes (start and end) with a link between,
+        # a span where they were on one team
+        _color = __current_career_phase["team_main_color"]
+        _color2 = __current_career_phase["team_secondary_color"]
+        _y_position_dictionary = {"team_name": __current_career_phase["nickname"],
+                                    "position_type_id": __current_career_phase["position_type_id"],
+                                    "position_id": __current_career_phase["position_id"]}
 
-            # first node
-            _x_position_dictionary, _label = self.node_x_and_labels_method(True, __current_career_phase)
-            _node_labels.append(_label)
-            _node_x_position_dictionaries.append(_x_position_dictionary)
-            _node_y_position_dictionaries.append(_y_position_dictionary)
-            _node_colors.append(_color)
-            _link_sources.append((2*_phase_count) + _index_of_last_node_added)
-            _link_targets.append((2*_phase_count) + _index_of_last_node_added + 1)
+        # first node
+        _x_position_dictionary, _label = create_x_axis_nodes_and_labels(True, __current_career_phase)
+        _node_labels.append(_label)
+        _node_x_position_dictionaries.append(_x_position_dictionary)
+        _node_y_position_dictionaries.append(_y_position_dictionary)
+        _node_colors.append(_color)
+        _link_sources.append((2*_phase_count) + _index_of_last_node_added)
+        _link_targets.append((2*_phase_count) + _index_of_last_node_added + 1)
+        _link_values.append(1)
+        _link_colors.append(_color2)
+
+        # second node
+        _x_position_dictionary, _label = create_x_axis_nodes_and_labels(False, __current_career_phase)
+        _node_labels.append(_label)
+        _node_x_position_dictionaries.append(_x_position_dictionary)
+        _node_y_position_dictionaries.append(_y_position_dictionary)
+        _node_colors.append(_color)
+        # if this is the last node in the players career, don't add a link to the next node
+        if (_phase_count + 1) < len(__career_phases):
+            _link_sources.append((2*_phase_count) + _index_of_last_node_added + 1)
+            _link_targets.append((2*_phase_count) + _index_of_last_node_added + 2)
             _link_values.append(1)
             _link_colors.append(_color2)
+        _phase_count += 1
+    _new_index = _index_of_last_node_added + len(_node_labels)
 
-            # second node
-            _x_position_dictionary, _label = self.node_x_and_labels_method(False, __current_career_phase)
-            _node_labels.append(_label)
-            _node_x_position_dictionaries.append(_x_position_dictionary)
-            _node_y_position_dictionaries.append(_y_position_dictionary)
-            _node_colors.append(_color)
-            # if this is the last node in the players career, don't add a link to the next node
-            if (_phase_count + 1) < len(self.__career_phases):
-                _link_sources.append((2*_phase_count) + _index_of_last_node_added + 1)
-                _link_targets.append((2*_phase_count) + _index_of_last_node_added + 2)
-                _link_values.append(1)
-                _link_colors.append(_color2)
-            _phase_count += 1
-        _new_index = _index_of_last_node_added + len(_node_labels)
+    return {"node_labels": _node_labels,
+            "link_sources": _link_sources,
+            "link_targets": _link_targets,
+            "link_values": _link_values,
+            "link_colors": _link_colors,
+            "node_x_position_dictionaries": _node_x_position_dictionaries,
+            "node_y_position_dictionaries": _node_y_position_dictionaries,
+            "node_colors": _node_colors,
+            "new_index": _new_index}
 
-        return {"node_labels": _node_labels,
-                "link_sources": _link_sources,
-                "link_targets": _link_targets,
-                "link_values": _link_values,
-                "link_colors": _link_colors,
-                "node_x_position_dictionaries": _node_x_position_dictionaries,
-                "node_y_position_dictionaries": _node_y_position_dictionaries,
-                "node_colors": _node_colors,
-                "new_index": _new_index}
+class PlayerCareer:
+    def __init__(self, player_id, player_name, career_phase):
+        self.__player_id = player_id
+        self.__player_name = player_name
+        self.__player_team_names = [career_phase["nickname"]]
+        self.__career_phases = [career_phase]
 
+    def append_career_phase(self, career_phase):
+        self.__career_phases.append(career_phase)
+        self.__player_team_names.append(career_phase["nickname"])
+
+    def get_player_name(self):
+        return self.__player_name
+        
+    def get_career_phases(self):
+        return self.__career_phases
+
+    def was_player_ever_on_team(self, team_searched):
+        return team_searched in self.__player_team_names
 
 with open('all_roster_changes.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)  # create a dictionary with all the data in it by reading in the CSV file
@@ -169,7 +170,7 @@ unique_season_and_day_list = []
 for player in players_index:
     career = player["player_career"]
     if career.was_player_ever_on_team(team_to_display):
-        node_export = career.export_nodes(index_of_last_node_added, current_season_and_day)
+        node_export = export_nodes(career.get_career_phases(), index_of_last_node_added, current_season_and_day)
         node_x_position_dictionaries = node_export["node_x_position_dictionaries"]
         for node_dictionary in node_x_position_dictionaries:
             season_and_day = [node_dictionary.get("season"), node_dictionary.get("day")]
@@ -191,7 +192,7 @@ for player in players_index:
         was_ever_not_on_team = False
         # take our big node_export data bundle and break it into the parts we need to pass to Plotly
         # and append those to the long lists Plotly takes as input
-        node_export = career.export_nodes(index_of_last_node_added, current_season_and_day)
+        node_export = export_nodes(career.get_career_phases(), index_of_last_node_added, current_season_and_day)
         node_labels.extend(node_export["node_labels"])
         node_colors.extend(node_export["node_colors"])
         link_sources.extend(node_export["link_sources"])
